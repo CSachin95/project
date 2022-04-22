2d57e704019518d597c9d6050d7e9a31e90a5552
John Jones <JLJ16@email.sc.edu>
2022-04-22 14:53:21 -0400

    Who even knows...

--- Main.cpp
+++ Main.cpp
@@ -3,55 +3,42 @@
 #include "Plotter.h"
 
 #include "VoltageSource.h"
-#include "Diode.h"
 #include "Resistor.h"
 #include "Capacitor.h"
 #include "Battery.h"
 
-/*
-  Example circuit (rectifier):
-	 (1)   D1      (2)
-	   .--->|---.-------.
-	  +|        |       |     +
-  vin (~)    R1 <   C1 ===   vout
-	   |        |       |     -
-	   '--------+-------'
-			   -+- (0)
-				'
-  time step = 1e-5 s
-  simulation time = 5 ms
 
-  vin = 10 * sin(2*pi*1000*t)
-  R1 = 10 Ohm
-  C1 = 1.0 mF
-*/
 
-int main()
+int main(void)
 {
 	const double h = 1e-6;
 	const double tmax = 5e-3;
 	const double Va = 10;
 	const double f = 1000;
-	const double R1 = GetR1(SOC);
-	const double R2 = GetR2(SOC);
-	const double R3 = GetR3(SOC);
-	const double C1 = GetC1(soc);
-	const double C2 = GetC2(soc);
+	const double Ra = R1;
+	const double Rb = R2;
+	const double Rc = R3;
+	const double Ca = C1;
+	const double Cb = C2;
 
 	Plotter plotter("Project", 1000, 600);
 	plotter.SetLabels("vin (V)", "iBatt (A)", "vout (V)");
 
-	Simulator simulator(2, 0);
+	Simulator simulator(nodepos, nodeneg);
 
-	VoltageSource V1(1, 0, 0, Va, f);
-	Diode D1(1, 2);
-	Resistor R1(2, 0, R);
-	Capacitor C1(2, 0, C);
+	VoltageSource V1(int1, nodeneg, 0, Va, f);
+	Resistor R1(int1, int2, Ra);
+	Resistor R2(int2, int3, Rb);
+	Resistor R3(int3, nodepos, Rc);
+	Capacitor C1(int2, int3, Ca);
+	Capacitor C2(int3, nodepos, Cb);
 
 	simulator.AddDevice(V1);
-	simulator.AddDevice(D1);
 	simulator.AddDevice(R1);
+	simulator.AddDevice(R2);
+	simulator.AddDevice(R3);
 	simulator.AddDevice(C1);
+	simulator.AddDevice(C2);
 
 	simulator.Init(h, tmax);
 

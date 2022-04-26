e96057433bb5675d1473eded8dd2f595748381fa
John Jones <JLJ16@email.sc.edu>
2022-04-26 19:56:09 -0400

    Correct Testing Parameters

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,7 @@
 int main()
 {
 	const double h = 0.01; //Timestep 0.1sec
-	const double tmax = 10.0; //Run time 10 sec
+	const double tmax = 20.0; //Run time 10 sec
 
 	Plotter plotter("Project", 1000, 600);
 	plotter.SetLabels("batt SOC", "Rload Current", "Rload Voltage");
@@ -18,7 +18,7 @@ int main()
 	Simulator simulator(1, 0);
 	
 	Battery batt(1, 0, 0.9); //SOC 90%
-	Resistor Rload(1, 0, 0.1); //Rload 0.1 Ohms 
+	Resistor Rload(1, 0, 0.5); //Rload 0.1 Ohms 
 
 	simulator.AddDevice(batt);
 	simulator.AddDevice(Rload);
